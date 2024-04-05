883496f046b0ee2b7bc824a0ea4ab07f9910cfce
Rebecca Kariuki <97875510+Beckyshee@users.noreply.github.com>
2024-04-05 23:32:05 +0300

    my first react typescript application

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -4,12 +4,19 @@ import logo from '../assets/pcea-logo.jpg'
 
 const Navbar = () => {
   return (
-      <div className="navbar-container">
-          <div className="navbar-logo">
-              <img src={logo} alt="logo"  />
+    <div className="navbar-container">
+      <div className="navbar-logo">
+        <img src={logo} alt="logo" />
+        <div className="heading">
+          <h3>PCEA</h3>
+          <h3>RURINGU CHURCH</h3>
+        </div>
+          </div>
+          <div className="navbar-links">
+              <a href="#">Home</a>
           </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
