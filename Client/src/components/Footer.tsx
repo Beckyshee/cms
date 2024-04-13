d3159ee4e1e2ba29ad42141fb39ca03dc1bb7a84
Rebecca Kariuki <97875510+Beckyshee@users.noreply.github.com>
2024-04-13 22:30:28 +0300

    styling

--- Client/src/components/Footer.tsx
+++ Client/src/components/Footer.tsx
@@ -2,53 +2,59 @@ import logo from '../assets/pcea-logo.jpg';
 import { FaPhone } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { MdLocationOn } from "react-icons/md";
+import './footer.scss'
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <h3>FOOTER COMPONET</h3>
-      <div className="footer-logo">
-        <img src={logo} alt="logo" className="logo" />
-        <div className="heading">
-          <h3>PCEA</h3>
-          <h3>RURINGU CHURCH</h3>
+      <div className="footer-cards">
+        <h3>FOOTER COMPONET</h3>
+        <div className="footer-logo">
+          <img src={logo} alt="logo" className="logo" />
+          <div className="heading">
+            <h3>PCEA</h3>
+            <h3>RURINGU CHURCH</h3>
+          </div>
         </div>
-      </div>
 
-      <div className="contactus">
-        <div className="header">CONTACT US</div>
-        <div className="contact-details">
-          <div className="contact-">
-            <FaPhone />
-            <p>07123456789</p>
+        <div className="contactus">
+          <div className="header">CONTACT US</div>
+          <div className="contact-details">
+            <div className="contact-">
+              <FaPhone />
+              <p>07123456789</p>
+            </div>
+            <div className="contact-">
+              <IoMdMail />
+              <p>pcearuringu@gmail.com</p>
+            </div>
+            <div className="contact-">
+              <MdLocationOn />
+              <p>Ruringu skuta opp waka</p>
+            </div>
+            <div className="contact-">
+              <p>Office Hours</p>
+              <p>
+                9:00am - 5:00pm <br />
+                Monday - Saturday
+              </p>
+            </div>
+            items
           </div>
-          <div className="contact-">
-            <IoMdMail />
-            <p>pcearuringu@gmail.com</p>
-          </div>
-          <div className="contact-">
-            <MdLocationOn />
-            <p>Ruringu skuta opp waka</p>
-          </div>
-          <div className="contact-">
-            <p>Office Hours</p>
-            <p>9:00am - 5:00pm Monday - Saturday</p>
-          </div>
-          items
         </div>
-      </div>
 
-      <div className="givings">
-        <div className="givings-details">
-          <div className="header">
-            <h3>GIVE</h3>
-          </div>
-          <div className="details">
-            <p>Mpesa: 303030</p>
-            <p>AccountName: Tithe, Offering,ThanksGiving</p>
-            <p>Bank: 235654666</p>
-            <p>Branch: Nyeri</p>
-            <p></p>
+        <div className="givings">
+          <div className="givings-details">
+            <div className="header">
+              <h3>GIVE</h3>
+            </div>
+            <div className="details">
+              <p>Mpesa: 303030</p>
+              <p>AccountName: Tithe, Offering,ThanksGiving</p>
+              <p>Bank: 235654666</p>
+              <p>Branch: Nyeri</p>
+              <p></p>
+            </div>
           </div>
         </div>
       </div>
